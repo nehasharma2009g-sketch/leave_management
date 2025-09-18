ede19315c742895c456da6056e93c3a1cdb5d25a
nehasharma2009g-sketch <nehasharma2009g@gmail.com>
2025-09-18 13:15:19 +0530

    changed

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-for(let i =0; i<30;i++){
-    console.log('doneeeeee')
-    
-}
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    for (let i = 0; i < 30; i++) {
+        console.log('doneeeeee'); // This will print in the server's console
+    }
+    res.send('Done 30 times! Check the server console for logs.');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
